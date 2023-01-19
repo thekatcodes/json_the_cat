e3218d9f61d55545cebae37042192b1b056120f2
Katie Liu <katieliu59@gmail.com>
2023-01-19 18:59:46 -0500

    Install Mocha and Chai for testing, refactor code to solve assignment problems

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,33 +1,23 @@
 const request = require("request");
 
-let breedName = [];
-for (let arg = 0; arg < process.argv.length - 2; arg++) {
-	breedName.push(process.argv[arg + 2]);
-}
 // console.log(breedName[0])
 const fetchBreedDescription = function(breedName, callback) {
-    
-    request(
-        `https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`,
-        function (error, response, body) {
-            "error:", error; // Print the error if one occurred
-            // console.log(error)
-            "statusCode:", response && response.statusCode; // Print the response status code if a response was received
-            if (error !== null) {
-                console.log("Request failed");
-                return;
-            }
-            "body:", body; // Print the HTML for the Google homepage.
-            const data = JSON.parse(body);
-            // console.log(data);
-    
-            if (data.length === 0) {
-                console.log("Requested breed is not found");
-            } else {
-                console.log(data[0].description);
-            }
-        }
-    );
-}
+  const searchURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-module.exports = {fetchBreedDescription}
+  request(searchURL, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+
+      if (data.length === 0) {
+        callback("Breed not found", null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const { fetchBreedDescription } = require('./breedFetcher');
+const { fetchBreedDescription } = require("./breedFetcher");
 
-let breedName = [];
-for (let arg = 0; arg < process.argv.length - 2; arg++) {
-	breedName.push(process.argv[arg + 2]);
-}
+const breedName = process.argv[2];
 
-fetchBreedDescription(breedName, function(error, desc) {
-    if (error) {
-        console.log('Error fetch details:', error);
-    } else {
-        console.log(desc);
-    }
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
 });
