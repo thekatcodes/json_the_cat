147d661ed4ca16ff35927b9ea35d730b5b573f92
Katie Liu <katieliu59@gmail.com>
2023-01-19 18:16:14 -0500

    Use TheCatAPI to fetch data for user according to cat breed name, implement logic to retrieve the description of the cat breed and log messaged when the breed is not found or the request failed

---
+++ breedFetcher.js
@@ -0,0 +1,29 @@
+const request = require("request");
+
+let breedName = [];
+for (let arg = 0; arg < process.argv.length - 2; arg++) {
+	breedName.push(process.argv[arg + 2]);
+}
+// console.log(breedName[0])
+
+request(
+	`https://api.thecatapi.com/v1/breeds/search?q=${breedName[0]}`,
+	function (error, response, body) {
+		"error:", error; // Print the error if one occurred
+		// console.log(error)
+		"statusCode:", response && response.statusCode; // Print the response status code if a response was received
+        if (error !== null) {
+            console.log("Request failed");
+            return;
+        }
+		"body:", body; // Print the HTML for the Google homepage.
+		const data = JSON.parse(body);
+		// console.log(data);
+
+        if (data.length === 0) {
+			console.log("Requested breed is not found");
+        } else {
+			console.log(data[0].description);
+		}
+	}
+);
